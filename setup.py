79c4e2e7442190a8813bbbf4940ecf2d96bae51d
zhelev <krasimir.zhelev@fau.de>
2017-01-29 21:02:30 +0100

    simplifying the setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,15 @@
-# pylint: disable=invalid-name, exec-used
 """Setup fsapi package."""
-from __future__ import absolute_import
-import sys
-import os
-from setuptools import setup, find_packages
-# import subprocess
-sys.path.insert(0, '.')
 
-CURRENT_DIR = os.path.dirname(__file__)
-
-# to deploy to pip, please use
-# make pythonpack
-# python setup.py register sdist upload
-# and be sure to test it firstly using "python setup.py register sdist upload -r pypitest"
 setup(name='fsapi',
       version='0.0.1',
-      description=open(os.path.join(CURRENT_DIR, 'README.md')).read(),
+      description='Implementation of the Frontier Silicon API for python',
       install_requires=['requests','lxml'],
       maintainer='Krasimir Zhelev',
       maintainer_email='krasimir.zhelev@gmail.com',
       author='Krasimir Zhelev',
       author_email='krasimir.zhelev@gmail.com',
       zip_safe=False,
-      packages=['fsapi'], #find_packages(),
+      packages=['fsapi'],
       include_package_data=True,
       download_url = 'https://github.com/zhelev/python-fsapi/archive/master.zip',
       url='https://github.com/zhelev/python-fsapi.git',
