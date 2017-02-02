a716e10833f644720838423f106e53ccac041014
zhelev <krasimir.zhelev@fau.de>
2017-02-02 17:47:18 +0100

    cleaned up code according to flake8

--- setup.py
+++ setup.py
@@ -3,16 +3,11 @@ from distutils.core import setup
 setup(name='fsapi',
       version='0.0.3',
       description='Implementation of the Frontier Silicon API for python',
-      #install_requires=['requests','lxml'],
-      #maintainer='Krasimir Zhelev',
-      #maintainer_email='krasimir.zhelev@gmail.com',
+      # install_requires=['requests','lxml'],
       author='Krasimir Zhelev',
       author_email='krasimir.zhelev@gmail.com',
-      #zip_safe=False,
       packages=['fsapi'],
-      #include_package_data=True,
       keywords='fsapi frontier silicon',
       license='Apache 2',
       download_url='https://github.com/zhelev/python-fsapi/archive/0.0.3.zip',
-      url='https://github.com/zhelev/python-fsapi.git',
-)
+      url='https://github.com/zhelev/python-fsapi.git')
