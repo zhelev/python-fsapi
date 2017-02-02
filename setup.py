3868f2a62e43ac3199eb996e2b904dcfbdb0ff73
zhelev <krasimir.zhelev@fau.de>
2017-02-02 19:07:43 +0100

    version bump and moved to setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 """
 Setup file for the fsapi package
 """
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='fsapi',
-      version='0.0.4',
+      version='0.0.5',
       description='Implementation of the Frontier Silicon API for python',
-      # install_requires=['requests','lxml'],
+      install_requires=['requests', 'lxml'],
       author='Krasimir Zhelev',
       author_email='krasimir.zhelev@gmail.com',
       packages=['fsapi'],
       keywords='fsapi frontier silicon',
       license='Apache 2',
-      download_url='https://github.com/zhelev/python-fsapi/archive/0.0.3.zip',
-      url='https://github.com/zhelev/python-fsapi.git')
+      download_url='https://github.com/zhelev/python-fsapi/archive/0.0.5.zip',
+      url='https://github.com/zhelev/python-fsapi.git',
+      maintainer='Krasimir Zhelev',
+      maintainer_email='krasimir.zhelev@gmail.com',
+      zip_safe=False,
+      include_package_data=True)
