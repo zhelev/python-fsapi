7075858068fe8a9a5adce7553977078abe3d56e0
zhelev <krasimir.zhelev@fau.de>
2017-02-05 13:07:56 +0100

    fixes for classifiers and requirements

--- setup.py
+++ setup.py
@@ -1,20 +1,47 @@
 """
 Setup file for the fsapi package
 """
-from setuptools import setup
+from setuptools import setup, find_packages
+
+PACKAGES = find_packages(exclude=['tests', 'tests.*'])
+
+REQUIRES = [
+    'requests>=2,<3'
+    'lxml>=3,<4'
+]
+
+PROJECT_CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.0',
+    'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Software Development :: Libraries'
+]
+
+
 
 setup(name='fsapi',
-      version='0.0.5',
-      description='Implementation of the Frontier Silicon API for python',
-      install_requires=['requests', 'lxml'],
+      version='0.0.6',
+      description='Implementation of the Frontier Silicon API for Python',
       author='Krasimir Zhelev',
       author_email='krasimir.zhelev@gmail.com',
-      packages=['fsapi'],
       keywords='fsapi frontier silicon',
-      license='Apache 2',
-      download_url='https://github.com/zhelev/python-fsapi/archive/0.0.5.zip',
+      license="Apache License 2.0",
+      download_url='https://github.com/zhelev/python-fsapi/archive/0.0.6.zip',
       url='https://github.com/zhelev/python-fsapi.git',
       maintainer='Krasimir Zhelev',
       maintainer_email='krasimir.zhelev@gmail.com',
-      zip_safe=False,
-      include_package_data=True)
+      zip_safe=True,
+      include_package_data=True,
+      packages=PACKAGES,
+      platforms='any',
+      install_requires=REQUIRES,
+      classifiers=PROJECT_CLASSIFIERS,
+     )
